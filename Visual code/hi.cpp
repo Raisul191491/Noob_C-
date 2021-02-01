3ce698ca763dab2a3d95f8dbfd1a755161e1857b
Md. Raisul Islam <52915635+Raisul191491@users.noreply.github.com>
2021-02-02 00:24:01 +0600

    setup

--- Visual code/hi.cpp
+++ Visual code/hi.cpp
@@ -16,66 +16,10 @@ typedef long long     ll;
 #define pi            3.14159265358979323846264338327950
 #define endl          '\n'
 using namespace std;
-map<unsigned long long,ll>mp;
-//map<pair<ll,ll>,ll>sp;
-vector<pair<ll,ll>>vec;
-
-void pro(unsigned long long x)
-{
-    if(mp[x])
-        return;
-    else
-    {
-        mp[x]++;
-        ll a;
-        for(ll i=sqrt(x); i<=sqrt(x)+2; i++)
-        {
-            a=pow(i,2)-x;
-            if(x==123456789)
-            {
-                mp[123456789]++;
-                vec.push_back({11112,19755});
-                pro(11112);
-                pro(19755);
-            }
-
-            else if(a>=0)
-            {
-                //cout << pow(i,2) << endl;
-//                if(sp[ {i,a}]==0 && sp[ {a,i}]==0)
-//                {
-                vec.push_back({i,a});
-//                    sp[ {i,a}]++;
-//                }
-                pro(a);
-                pro(i);
-                break;
-            }
-        }
-    }
-}
-
 int main()
 {
-    mp[0]=1;
-    mp[1]=1;
-    mp[2]=1;
-    unsigned long long n;
-    cin>>n;
-//     if(n==1 || n==2 || n==0)
-//     {
-//          if(n==0)
-//               cout << "1 1" << endl;
-//          else if(n==1)
-//               cout << "1 0" << endl;
-//          else
-//               cout << "2 2" << endl;
-//          return 0;
-//     }
-    pro(n);
-    sortvi(vec);
-    for(int i=0; i<vec.size(); i++)
-    {
-        cout << vec[i].fr << " " << vec[i].sc << endl;
-    }
+    int a;
+    cin>> a;
+    cout << "The ans is:" << a << endl;
+    vector<long long int>soft;          
 }
